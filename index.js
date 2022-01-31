d5bd583b7ba29fdfaf794aa7e49dc33012079a8b
FraserDouglas <fraser.douglas@transportforthenorth.com>
2022-01-31 14:59:05 +0000

    working openlayers amazon s3 solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//HTTP handling
+//HTTP Handling
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require("path");
@@ -24,7 +24,7 @@ const config = {
 const s3 = new AWS.S3(config);
 const getParams = {
     Bucket: bucketName, 
-    Key: "Countries_(December_2020)_UK_BFE.geojson"
+    Key: "Lower_Layer_Super_Output_Areas_(December_2011)_Boundaries_Super_Generalised_Clipped_(BSC)_EW_V3.geojson"
 };
 
 // create a new express app instance
@@ -45,20 +45,14 @@ app.get('/tileserver', (req, res) => {
     const z = parseInt(req.query.z);
     const x = parseInt(req.query.x);
     const y = parseInt(req.query.y.replace('.pbf', ''));
-    console.log(req.query, " mapping library is sending this info everytime map moves to different tile")
-    console.log(x,y,z, "Tile Coordinates to look up in index")
     const tile = tileIndex.getTile(z, x, y);
-    // console.log(tile)
     if (!tile) {
         return res.status(204).end();
     }
-    console.log(tile, "Specific Tile to send to mapping library, needs to be converted to pbf")
-
     // encode the data as protobuf
     const buffer = Buffer.from(vtpbf.fromGeojsonVt({ geojsonLayer: tile }));
-    console.log(typeof(buffer), "This is the final buffer data sent back to mapping library to be rendered.")
     res.send(buffer);
-});
+});    
 
 /*open the connection to S3 and get 
 the data from the bucket*/
@@ -67,11 +61,8 @@ s3.getObject(getParams, (error, data) => {
     else {
         let jsonData = data.Body.toString('utf-8');
         jsonData = JSON.parse(jsonData);
-        console.log(jsonData)
         // we tile the full geojson file
         tileIndex = geojsonvt(jsonData);
-        console.log(tileIndex)
-        // console.log(tileIndex)
         app.listen(PORT, () => {
             console.log('App is listening on port 5000!');
         });
