e383f9c82177cdf1a9c27ae94b32e6d8ad7d1404
FraserDouglas <fraser.douglas@transportforthenorth.com>
2022-02-02 15:58:55 +0000

    initial commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const vtpbf = require('vt-pbf');
 // set the AWS region
 const REGION = "eu-west-2"; // e.g., "us-east-1"
 // set the bucket parameters
-const bucketName = 'vector-tiles-tame';
+const bucketName = 'www.tame-vector-tiles.xyz';
 // create S3 initial config for connection
 const config = {
     apiVersion: '2006-03-01',
@@ -22,10 +22,6 @@ const config = {
     region: REGION
 };
 const s3 = new AWS.S3(config);
-const getParams = {
-    Bucket: bucketName, 
-    Key: "Countries_(December_2020)_UK_BFE.geojson"
-};
 
 // create a new express app instance
 const app = express();
@@ -39,41 +35,69 @@ want to handle response in JSON formats later on */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-let tileIndex;
+
+// const getParams = {
+//     Bucket: bucketName, 
+//     // Key: `LSOA_Tiles/${z}/${x}/${y}.pbf`
+//     Key: `LSOA_Tiles/5/15/10.pbf`
+
+// };
+// s3.getObject(getParams, (error, data) => {
+//     if (error) console.log(error, error.stack);
+//     else {
+//         // let jsonData = data.Body.toString('utf-8');
+//         // jsonData = JSON.parse(jsonData);
+//         // console.log(jsonData)
+//         console.log(data.Body)
+//         // we tile the full geojson file
+//         // tileIndex = geojsonvt(jsonData);
+//         // console.log(tileIndex)
+//         // console.log(tileIndex)
+//         // app.listen(PORT, () => {
+//         //     console.log('App is listening on port 5000!');
+//         // });
+//     }
+// })
+
+
+
 // route for the Vector Tiles request
 app.get('/tileserver', (req, res) => {
     const z = parseInt(req.query.z);
     const x = parseInt(req.query.x);
     const y = parseInt(req.query.y.replace('.pbf', ''));
-    console.log(req.query, " mapping library is sending this info everytime map moves to different tile")
     console.log(x,y,z, "Tile Coordinates to look up in index")
-    const tile = tileIndex.getTile(z, x, y);
-    // console.log(tile)
-    if (!tile) {
-        return res.status(204).end();
-    }
-    console.log(tile, "Specific Tile to send to mapping library, needs to be converted to pbf")
-
-    // encode the data as protobuf
-    const buffer = Buffer.from(vtpbf.fromGeojsonVt({ geojsonLayer: tile }));
-    console.log(typeof(buffer), "This is the final buffer data sent back to mapping library to be rendered.")
-    res.send(buffer);
-});
-
-/*open the connection to S3 and get 
+    /*open the connection to S3 and get 
 the data from the bucket*/
+
+const getParams = {
+    Bucket: bucketName, 
+    Key: `LSOA_Tiles/${z}/${x}/${y}.pbf`
+};
 s3.getObject(getParams, (error, data) => {
-    if (error) console.log(error, error.stack);
+    if (error) {
+        console.log(error, error.stack)
+        return res.status(204).end()
+    }
     else {
         let jsonData = data.Body.toString('utf-8');
         jsonData = JSON.parse(jsonData);
         console.log(jsonData)
         // we tile the full geojson file
-        tileIndex = geojsonvt(jsonData);
-        console.log(tileIndex)
+        // tileIndex = geojsonvt(jsonData);
         // console.log(tileIndex)
-        app.listen(PORT, () => {
-            console.log('App is listening on port 5000!');
-        });
+        // console.log(tileIndex)
+        // app.listen(PORT, () => {
+        //     console.log('App is listening on port 5000!');
+        // });
     }
 });
+
+    console.log(tile, "Specific Tile to send to mapping library, needs to be converted to pbf")
+
+    // encode the data as protobuf
+    const buffer = Buffer.from(vtpbf.fromGeojsonVt({ geojsonLayer: tile }));
+    console.log(buffer, "This is the final buffer data sent back to mapping library to be rendered.")
+    res.send(buffer);
+});
+
