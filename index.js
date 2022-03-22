0b35f7f1601e2e52f4db2bd1b3795b304e478934
FraserDouglas <fraser.douglas@transportforthenorth.com>
2022-03-22 15:11:42 +0000

    tidy

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
-const { v1: uuidv1 } = require("uuid");
-const style_light = require("./style_light.json");
-//HTTP handling
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 
-// create a new express app instance
 const app = express();
 app.use(cors());
 
@@ -20,21 +16,16 @@ app.listen(PORT, () => {
   console.log("App is listening on port 5000");
 });
 
-// AZURE CONNECTION
-
 const AZURE_STORAGE_CONNECTION_STRING =
   process.env.AZURE_STORAGE_CONNECTION_STRING;
 
 if (!AZURE_STORAGE_CONNECTION_STRING) {
   throw Error("Azure Storage Connection string not found");
 }
-// Create the BlobServiceClient object which will be used to create a container client
 const blobServiceClient = BlobServiceClient.fromConnectionString(
   AZURE_STORAGE_CONNECTION_STRING
 );
-// Create a unique name for the container
 const containerName = "vector-tiles";
-// Get a reference to a container
 const containerClient = blobServiceClient.getContainerClient(containerName);
 
 async function streamToBuffer(readableStream) {
@@ -50,16 +41,29 @@ async function streamToBuffer(readableStream) {
   });
 }
 
-// Style url
 app.get("/tileserver/style.json", (req, res) => {
   console.log("Style has been requested by map");
-  res.send(style_light);
-});
-// get Sprites
+  const blobName = `basemap/tiles/style_light.json`;
+  console.log({ blobName });
+ 
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+ 
+  blockBlobClient
+    .download(0)
+    .then((downloadBlockBlobResponse) => {
+      return streamToBuffer(downloadBlockBlobResponse.readableStreamBody);
+    })
+    .then((data) => {
+      console.log({ data });
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log("notfound");
+    });
+ })
 
 app.get("/tileserver/basemap/sprites/sprites.json", (req, res) => {
   console.log("Sprites.json have been requested by map")
- // AZURE CALL GOES HERE
  const blobName = `basemap/sprites/sprites.json`;
  console.log({ blobName });
 
@@ -82,7 +86,6 @@ app.get("/tileserver/basemap/sprites/sprites.json", (req, res) => {
 app.get("/tileserver/basemap/sprites/sprites.png", (req, res) => {
   console.log("Sprites.png have been requested by map")
 
- // AZURE CALL GOES HERE
  const blobName = `basemap/sprites/sprites.png`;
  console.log({ blobName });
 
@@ -101,7 +104,6 @@ app.get("/tileserver/basemap/sprites/sprites.png", (req, res) => {
      console.log("notfound");
    });
 })
-// fonts url
 app.get("/tileserver/basemap/fonts/:fontstack/:range", (req, res) => {
   console.log("FONT REQUEST MADE");
 
@@ -109,7 +111,6 @@ app.get("/tileserver/basemap/fonts/:fontstack/:range", (req, res) => {
   const range = req.params.range.replace(".pbf", "");
   console.log("FONT REQUEST MADE", fontstack, range);
 
-  // AZURE CALL GOES HERE
   const blobName = `basemap/fonts/${fontstack}/${range}.pbf`;
   console.log({ blobName });
 
@@ -128,7 +129,6 @@ app.get("/tileserver/basemap/fonts/:fontstack/:range", (req, res) => {
       console.log("notfound");
     });
 });
-// Tiles url
 app.get("/tileserver/:tileName/tiles/:z/:x/:y", (req, res) => {
   const z = parseInt(req.params.z);
   const x = parseInt(req.params.x);
@@ -137,7 +137,6 @@ app.get("/tileserver/:tileName/tiles/:z/:x/:y", (req, res) => {
 
   console.log(z, x, y, tileName, "Tile Coordinates to look up in index");
 
-  // AZURE CALL GOES HERE
   const blobName = `${tileName}/tiles/${z}/${x}/${y}.pbf`;
   console.log({ blobName });
 
--- indexGITGUIDE.js
+++
@@ -1,9 +0,0 @@
-var azure = require('azure-storage');
-var fs = require('fs');
-
-const AZURE_STORAGE_ACCOUNT =
-blobSvc.getBlobToStream('mycontainer', 'myblob', fs.createWriteStream('output.txt'), function(error, result, response){
-  if(!error){
-    // blob retrieved
-  }
-});
