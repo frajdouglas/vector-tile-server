51dd554aa7b35e9cb76fa51ae74da6a54a0da0b7
FraserDouglas <fraser.douglas@transportforthenorth.com>
2022-04-04 09:39:42 +0100

    change to port 5000

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "../../build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
